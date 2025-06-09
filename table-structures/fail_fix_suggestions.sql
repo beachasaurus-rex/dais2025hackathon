d1047cafe0f2f77894b614c47a1cf8e2a57fbbca
beachasaurus-rex <jonrizk@protonmail.com>
2025-06-09 15:58:38 -0500

    wip

--- table-structures/fail_fix_suggestions.sql
+++
@@ -1,5 +0,0 @@
-create or replace table fail_fix_suggestions (
-    fail_fix_suggestions_key bigint generated always as identity,
-    fail_rsn_key bigint not null,
-    fix_suggestion string not null
-)
--- table-structures/fail_rsn.sql
+++
@@ -1,6 +0,0 @@
-create or replace table fail_rsn (
-    rsn_key bigint generated always as identity,
-    rsn_when timestamp not null,
-    rsn string not null,
-    notebook_path string not null
-)
